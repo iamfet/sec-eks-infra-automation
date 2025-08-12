a8c43bfa55bc43b5e92492a7d3ae4a66d0131709
iamfet <fetuyee@gmail.com>
2025-08-12 18:58:28 +0000

    feat: add providers

---
+++ providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket       = "state-eks-secure-infra-automation"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true                              # Enable S3 native state locking, remove if using DynamoDB for state locking
+    /*dynamodb_table = "terraform-eks-state-locks"*/ # Uncomment if using DynamoDB for state locking
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.95"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.38"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 3.0"
+    }
+  }
+}
