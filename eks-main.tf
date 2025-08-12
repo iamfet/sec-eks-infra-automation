dc691b1a56c390851fd133dc8b25a78db64ca4cf
iamfet <fetuyee@gmail.com>
2025-08-12 18:52:01 +0000

    feat: add eks main

---
+++ eks-main.tf
@@ -0,0 +1,122 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+#VPC for Cluster
+data "aws_availability_zones" "azs" {
+  state = "available"
+} #queries AWS to provide the names of availability zones dynamically
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.21" #6.0
+
+  name            = "${var.project_name}-vpc"
+  cidr            = var.vpc_cidr_block
+  private_subnets = var.private_subnets_cidr
+  public_subnets  = var.public_subnets_cidr
+  azs             = data.aws_availability_zones.azs.names
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "shared" # Tags required for EKS to discover subnets
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                                = 1 # Identifies this subnet for external load balancers
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.project_name}-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"                       = 1 # Identifies this subnet for internal services
+  }
+}
+
+#EKS for Cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.37" #21.0
+
+  cluster_name    = "${var.project_name}-eks-cluster"
+  cluster_version = var.cluster_version
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  cluster_endpoint_public_access = true
+
+  # Ensure proper dependency order
+  depends_on = [module.vpc, aws_iam_role.external-admin, aws_iam_role.external-developer]
+
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  # Set authentication mode to API
+  authentication_mode = "API"
+
+  # Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  # Add access entries
+  access_entries = {
+    admin = {
+      principal_arn = aws_iam_role.external-admin.arn
+      type          = "STANDARD"
+      access_scope = {
+        type = "cluster"
+      }
+    }
+
+    developer = {
+      principal_arn = aws_iam_role.external-developer.arn
+      type          = "STANDARD"
+      access_scope = {
+        type       = "namespace"
+        namespaces = ["online-boutique"]
+      }
+    }
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      instance_types = ["t2.large"]
+      min_size       = 1
+      max_size       = 4
+      desired_size   = 2
+
+      tags = {
+        "k8s.io/cluster-autoscaler/enabled"                         = "true"
+        "k8s.io/cluster-autoscaler/${var.project_name}-eks-cluster" = "owned"
+      }
+    }
+  }
+
+  tags = {
+    environment = "dev"
+    terraform   = true
+  }
+}
+
+# EBS CSI Driver IRSA
+module "ebs_csi_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name             = "${var.project_name}-ebs-csi"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
