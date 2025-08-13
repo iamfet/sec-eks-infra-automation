02e00c5e7a003ca4f4f8e52ae7f23f9b4d56a9eb
iamfet <fetuyee@gmail.com>
2025-08-13 16:04:35 +0000

    feat: add cert manager

---
+++ cert-manager.tf
@@ -0,0 +1,46 @@
+data "aws_route53_zone" "fetdevops" {
+  name = "fetdevops.com"
+}
+
+module "cert_manager_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.39.0"
+
+  role_name                     = "${var.project_name}-cert-manager-irsa"
+  attach_cert_manager_policy    = true
+  cert_manager_hosted_zone_arns = [data.aws_route53_zone.fetdevops.arn]
+
+  oidc_providers = {
+    eks = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["cert-manager:cert-manager"]
+    }
+  }
+}
+
+
+
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  version          = "v1.18.2"
+  namespace        = "cert-manager"
+  create_namespace = true
+  depends_on       = [module.eks, module.cert_manager_irsa, helm_release.aws-load-balancer-controller]
+
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = module.cert_manager_irsa.iam_role_arn
+    }
+  ]
+}
