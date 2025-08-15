5d7cbcc7bfe96024e4b5f670db0ce9ef09f20ede
iamfet <fetuyee@gmail.com>
2025-08-15 13:09:07 +0000

    ci: trigger ci

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "public_subnets_cidr" {
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "ama-shop"
+  default     = "carney-shop"
 }
 
 variable "kubernetes_version" {
